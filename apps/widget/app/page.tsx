32b91455b0fed3ad70b521abb1c728c1fafa49ed
liuxin85 <liuxin85@gmail.com>
2025-10-05 20:36:55 +0800

    feat: add tubo repo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,15 @@
-import { Button } from "@workspace/ui/components/button";
 import { add } from "@workspace/math/add";
+import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
+        <h1 className="text-2xl font-bold">Hello apps/widge</h1>
         <Button size="sm">Button</Button>
-        {add(1, 3)}
+        {add(1, 2)}
+        <Input placeholder="" />
       </div>
     </div>
   );
